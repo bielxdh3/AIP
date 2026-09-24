70aaa19aef9b1a38513c20c7db40cd78fd59735a
GabrielM <bielxdh3@gmail.com>
2026-09-24 16:54:35 -0400

    fix(android): use AGP built-in Kotlin

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,1 +1,3 @@
-plugins { id("com.android.application") version "9.4.1" apply false; id("org.jetbrains.kotlin.android") version "2.0.21" apply false }
+plugins {
+    id("com.android.application") version "9.4.1" apply false
+}
